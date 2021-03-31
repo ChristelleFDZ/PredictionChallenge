0ebdb9f930f43ef6545a8dad388fe79c69e05820
ChristelleFDZ <christelle.fernandez@etu.umontpellier.fr>
2021-03-31 10:31:57 +0200

    prediction part

--- src/prediction_algorithm.py
+++ src/prediction_algorithm.py
@@ -1,22 +1,96 @@
-import utils
+from download import download
+from sklearn import linear_model, metrics
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import numpy as np
-# import scikit learn databases
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
 
 
 
-frame=pd.read_table('resources\data.csv',skiprows=3, header=None,usecols=range(0,4), delimiter=',')
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQVtdpXMHB4g9h75a0jw8CsrqSuQmP5eMIB2adpKR5hkRggwMwzFy5kB-AIThodhVHNLxlZYm8fuoWj/pub?gid=2105854808&single=true&output=csv"
+path_target = "resources\data.csv"
+download(url, path_target, replace=True)
+
+frame=pd.read_table("resources\data.csv",skiprows=3, header=None,usecols=range(0,4), delimiter=',')
 frame.columns=["Date","Time","total","daily"]
 # define the columns names of the data then convert to dataframe
 
 df = frame
-# print the df and shape to get a better understanding of the data
-print(df.shape)
-print(df)
 
-data=df.to_numpy()
-print(data)
+df= df[df["Time"].isnull() ==False]
+
+
+def format_date(df):
+    time_formated = pd.to_datetime(df['Date'] + " "+ df["Time"], format = '%d/%m/%Y %H:%M:%S')
+    df['Date_formated']=time_formated
+    return df
+
+df = format_date(df)
+
+
+#df1 = df1[df1['Time']<= '09:00:00']
+
+
+df['Date']=pd.to_datetime(df['Date'])
+df['Date']=df['Date'].map(dt.datetime.toordinal)
+
+
+def timestr(tab1):
+     d =[]
+     cpt=0
+     for item in tab1:
+        time= datetime.strptime(item,'%H:%M:%S')
+        int_time= time.hour*60+time.minute #we ignore the seconds
+        d.insert(cpt,int_time)
+       
+        cpt+=1
+     ser = pd.Series(np.array(d))
+     return ser
+
+
+df['Time']=timestr(df['Time'])
+df= df[df["Time"].isnull() ==False]
+
+
+#we use two explanatory variables
+X=pd.DataFrame(np.c_[df['Time'],df['daily']],columns=['Time','daily'])
+Y= df['Date']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.03, random_state=5)
+
+X_labels=pd.DataFrame(np.c_[df['Time'],df['daily']],columns=['Time','daily'])
+Y_labels= df['Date_formated']
+
+X_labels_train, X_labels_test, Y_labels_train, Y_labels_test = train_test_split(X_labels, Y_labels, test_size = 0.03, random_state=5)
+
+lmodellineaire = LinearRegression()
+lmodellineaire.fit(X_train, Y_train)
+
+# Evaluation of training set
+y_train_predict = lmodellineaire.predict(X_train)
+rmse = (np.sqrt(mean_squared_error(Y_train, y_train_predict)))
+r2 = r2_score(Y_train, y_train_predict)
+ 
+ 
+# model evaluation for testing set
+y_test_predict = lmodellineaire.predict(X_test)
+rmse = (np.sqrt(mean_squared_error(Y_test, y_test_predict)))
+r2 = r2_score(Y_test, y_test_predict)
+ 
+
+X_test=np.arange(0,len(X_test),1)
+
+# Plot outputs
+plt.scatter( X_test,Y_test,  color='black')
+plt.plot( X_test,y_test_predict, color='blue', linewidth=3)
+#plt.yticks(Y_test,Y_labels_test) mathplolib hardly responsive
+plt.show()
+
+print('The result for the prediction is:')
+print(np.sqrt(metrics.mean_squared_error(Y_test, y_test_predict)))
 
